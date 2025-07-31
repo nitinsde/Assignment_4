220bfe3ab9cfb29daab0641894b3e4cf02a4285f
nitinsde <nitinbharti688@gmail.com>
2025-07-31 01:17:51 -0700

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,10 @@
+doc1 = "sample.txt"
+try:
+    doc1 = open("sample.txt", "r")
+    file_data = doc1.read()
+    print(file_data)
+
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' was not found.")
+
+
