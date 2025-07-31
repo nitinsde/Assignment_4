5c3ee1360b0418bc0eac08a598fb537b69ffe1b1
nitinsde <nitinbharti688@gmail.com>
2025-07-31 05:15:11 -0700

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -3,7 +3,7 @@ try:
     doc1 = open("sample.txt", "r")
     file_data = doc1.read()
     print(file_data)
-
+    doc1.close()
 except FileNotFoundError:
     print("Error: The file 'sample.txt' was not found.")
 
